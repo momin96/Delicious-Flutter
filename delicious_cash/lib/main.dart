c74642146298f9bc8f994b6206631eaacb22e9e8
Nasir <momin96@gmail.com>
2019-04-06 15:27:34 +0530

    Google sigin & Firebase Auth packages added

--- delicious_cash/lib/main.dart
+++ delicious_cash/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'LandingPage.dart';
+
 void main() {
   runApp(EntryPoint());
 }
@@ -19,32 +21,7 @@ class EntryPoint extends StatelessWidget {
   }
 }
 
-class LandingPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return androidScaffold();
-  }
 
 /**
  * ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDNYnV3ifwhT8WJoOCbsnTEySynkajZDcsW9I+rxo+++sI5KL8cGH6bLvGyU+CPyNO8PE42OPX9KPwIpzfZYRk5nKfmUjsno/SoeTF1J3a8jMe7pqvwG45jKgPQ2apzHHfY+96svxVEiTbvjvAVcNnwt3ya/SczJUfZFmuYtGwO+EkJCwF8lW0IkhAtnFotgrGDYQJSA7yGHq83u1OTqF8XA42guRK06k85MU+vplxZhpDFTEI4eq5nnv0hphuu9VbKp+4uwWGOlo6VB5uuUEPfi9Mwl1EaICunYmiLZCkTgRWos6K8vHlrAyXRuyZkdZWt9oSAPPKcQDEWSNIITwoBss63xMDbC2crkoKMzX+g3z9FJxVBhppES5ekNl14NiaZL5zf0GiWUMNoDOyPvrI+VlWCPmp9EtkNDKX09zTLuqT4dgfucXPH4pmZ3ZnWAjlc8gM0RElTo48si/Tvx5aFMQeB0YNPB5MSSGLMRc3T6Olsh3qvC0kxkjUddSCqWnlC6vhsW/qRis3EQzY0BwXoCUkxEY2qOBV01fPLgulq+GH/EOp4qtnz+X57z8BU5WBK1RJfrdkKaSRjX6Oh+1CO8TpN0Nd00Tf2MYRnbv/L6yqXEIVQ8Pa8AYB3/JZxDBP9DfC6fnPVCOs/xOOJ6WG6uWTfgWLbZZeDM7hbbYrEGQ==
  */
-
-  Widget androidScaffold() {
-    return Scaffold(
-//      appBar: AppBar(
-//        title: Text('Delicious Cash'),
-//      ),
-      body: Container(
-        child: ListView(
-          children: <Widget>[
-            Center(
-              child: Column(
-                children: <Widget>[Text('Declicious Cash')],
-              ),
-            )
-          ],
-        ),
-      ),
-    );
-  }
-}
